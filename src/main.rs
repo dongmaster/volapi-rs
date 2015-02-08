a213a302b23e68cdce956614e05deac754998f8a
Dongmaster <dongmaster@users.noreply.github.com>
2015-02-08 02:46:08 +0100

    added deps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate url;
 extern crate websocket;
+extern crate "rustc-serialize" as rustc_serialize;
 
 const BASE_URL = "https://volafile.io";
 const BASE_ROOM_URL = format!("{}/r/", BASE_URL);
