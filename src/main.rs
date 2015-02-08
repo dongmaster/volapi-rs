3e9f2a25d5a14a6417abb009f7dce32542415d9a
Dongmaster <dongmaster@users.noreply.github.com>
2015-02-08 02:42:55 +0100

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate url;
+extern crate websocket;
+
+const BASE_URL = "https://volafile.io";
+const BASE_ROOM_URL = format!("{}/r/", BASE_URL);
+const BASE_REST_URL = format!("{}/rest/", BASE_URL)
+
+fn main() {
+    println!("Hello, world!");
+}
