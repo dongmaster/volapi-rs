7c88849ffcca8221d1c924aa11c554703f13df97
Dongmaster <dongmaster@users.noreply.github.com>
2015-02-08 02:48:04 +0100

    fixed deps?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate url;
-extern crate websocket;
+extern crate "bare-websocket" as websocket;
 extern crate "rustc-serialize" as rustc_serialize;
+
+use websocket::{WebSocket, WSMessage};
+use url::Url;
 
 const BASE_URL = "https://volafile.io";
 const BASE_ROOM_URL = format!("{}/r/", BASE_URL);
