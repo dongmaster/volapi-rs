1fc587acc4b979fe8a3d9d9492b4034a7f8e3a8c
Dongmaster <dongmaster@users.noreply.github.com>
2015-02-08 03:38:34 +0100

    made things compilable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 extern crate url;
-extern crate "bare-websocket" as websocket;
+extern crate websocket;
 extern crate "rustc-serialize" as rustc_serialize;
 
 use websocket::{WebSocket, WSMessage};
 use url::Url;
-
-const BASE_URL = "https://volafile.io";
-const BASE_ROOM_URL = format!("{}/r/", BASE_URL);
-const BASE_REST_URL = format!("{}/rest/", BASE_URL)
+/*
+let base_url = "https://volafile.io";
+let BASE_ROOM_URL: &str = format!("{}/r/", BASE_URL);
+let BASE_REST_URL: &str = format!("{}/rest/", BASE_URL);
+* */
 
 fn main() {
-    println!("Hello, world!");
+    let url = Url::parse("ws://echo.websocket.org").unwrap();
+    let mut ws = WebSocket::new(url);
+    ws.connect().unwrap();
+    
+    let msg = WSMessage::text("Hello, World!");
+    
+    ws.send_message(&msg).unwrap();
 }
